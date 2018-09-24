d4b06a69e1b9a841335479900a199cc3795ceab6
José Francisco <jose.francisco.pees@gmail.com>
2018-09-24 13:49:25 -0500

    WIP: RaceSchedule component

--- src/containers/RaceSchedule.js
+++ src/containers/RaceSchedule.js
@@ -1,9 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { getSeasonRaces } from '../api';
+import RaceList from '../components/RaceList/RaceList';
 
 class RaceSchedule extends Component {
+  state = {
+    races: [],
+    seasonName: '',
+  }
+
+  componentDidMount() {
+    getSeasonRaces()
+      .then((results) => {
+        this.setState({
+          races: results.Races,
+          seasonName: results.season,
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    const { seasonName, races } = this.state;
     return (
-      <h1>RaceSchedule Page</h1>
+      <Fragment>
+        <h2> Season {seasonName} </h2>
+        <RaceList races={races} />
+      </Fragment>
     );
   }
 }
