9222bfcf371d993f57018691da2f781bb54d675c
José Francisco <jose.francisco.pees@gmail.com>
2018-09-24 12:13:27 -0500

    added: Menu to Header component, changed to class component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -6,6 +6,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+
+import Menu from './Menu';
 
 const styles = {
   root: {
@@ -20,23 +22,41 @@ const styles = {
   },
 };
 
-const Header = ({ classes }) => (
-  <div className={classes.root}>
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          className={classes.menuButton}
-          color="inherit"
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="title" color="inherit" className={classes.grow}>
-          F1 App
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </div>
-);
+class Header extends Component {
+  state = {
+    open: false,
+  };
+
+  toggleDrawer = (open) => {
+    this.setState({
+      open,
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              onClick={() => this.toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu open={open} toggleDrawer={this.toggleDrawer} />
+            <Typography variant="title" color="inherit" className={classes.grow}>
+              F1 App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
 
 Header.propTypes = {
   classes: PropTypes.object.isRequired,
